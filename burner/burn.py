8182f11c364272847a8c06d8d70ae087e2c99bfa
Doug Schaefer <dschaefer2@apple.com>
2020-03-08 14:27:37 -0400

    Ability to send data down the serial port.

--- burner/burn.py
+++ burner/burn.py
@@ -3,16 +3,36 @@
 import serial
 import time
 
+def write_word(ser, word):
+    ser.write([word & 0xff, word >> 8])
+
 with serial.Serial('/dev/cu.usbmodem14301', 115200) as ser:
     # Need to read the first byte to prime the channel
     c = ser.read()
 
     ser.write(b's')
-    ser.flush()
     line = ser.readline()
     print(line.decode().strip())
 
-    ser.write(b't')
-    ser.flush()
+    data = []
+    for i in range(256):
+        data.append(i)
+
+    length = len(data)
+    offset = 0
+
+    checksum = 0
+    for i in range(256):
+        checksum += checksum + data[i]
+        checksum = checksum & 0xffff
+
+    ser.write(b'w')
+    write_word(ser, length)
+    write_word(ser, offset)
+    write_word(ser, checksum)
+
+    for b in data:
+        ser.write([b])
+    
     line = ser.readline()
     print(line.decode().strip())
--- burner/burner.ino
+++ burner/burner.ino
@@ -1,9 +1,16 @@
+#include <stdlib.h>
+
+typedef unsigned int size_t;
+extern void * malloc(size_t size);
+
 #define BAUD_RATE 115200
 
 #define CLOCK_PIN 13
 #define RESET_PIN 12
 #define OE_PIN    11
 #define WE_PIN    10
+
+int data_pins[8] = { 2, 3, 4, 5, 6, 7, 8, 9 };
 
 void setup() {
     pinMode(CLOCK_PIN, OUTPUT);
@@ -19,7 +26,7 @@ void setup() {
     digitalWrite(WE_PIN, HIGH);
 
     Serial.begin(BAUD_RATE);
-    Serial.print('.');
+    Serial.write('.');
 }
 
 void reset_counter() {
@@ -30,6 +37,19 @@ void reset_counter() {
 void pulse_clock() {
     digitalWrite(CLOCK_PIN, HIGH);
     digitalWrite(CLOCK_PIN, LOW);
+}
+
+int n = 0;
+
+unsigned int read_byte() {
+    while (Serial.available() == 0) {
+        delay(1);
+    }
+    return Serial.read() & 0xff;
+}
+
+unsigned short read_word() {
+    return read_byte() | (read_byte() << 8);
 }
 
 void run_test() {
@@ -43,8 +63,37 @@ void run_test() {
     Serial.println("Test complete");
 }
 
+void write_file() {
+    unsigned short length = read_word();
+    unsigned short offset = read_word();
+    unsigned short checksum = read_word();
+
+    unsigned short mycheck = 0;
+    unsigned char * data = malloc(length);
+    for (unsigned short i = 0; i < length; i++) {
+        data[i] = read_byte();
+        mycheck += mycheck + data[i];
+        n++;
+    }
+
+    if (mycheck == checksum) {
+        Serial.print("Success!");
+    } else {
+        Serial.print("Bad transfer checksum = ");
+        Serial.print(mycheck);
+        Serial.print(".");
+    }
+    Serial.print(" length = ");
+    Serial.print(length);
+    Serial.print(", offset = ");
+    Serial.print(offset);
+    Serial.print(", checksum = ");
+    Serial.print(checksum);
+    Serial.println();
+}
+
 void loop() {
-    int c = Serial.read();
+    int c = read_byte();
     switch (c) {
         case -1:
             delay(100);
@@ -54,6 +103,9 @@ void loop() {
             break;
         case 't':
             run_test();
+            break;
+        case 'w':
+            write_file();
             break;
     }
 }
